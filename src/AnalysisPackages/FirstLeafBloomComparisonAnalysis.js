77bbba06762d0f689b64bcd707744b1028dd5d8a
Ben Gotthold <bgotthold@usgs.gov>
2019-03-06 10:25:08 -0700

    geom spanning 180, sb contacs on info, hide non obis baps

--- src/AnalysisPackages/FirstLeafBloomComparisonAnalysis.js
+++ src/AnalysisPackages/FirstLeafBloomComparisonAnalysis.js
@@ -105,7 +105,11 @@ class FirstLeafBloomComparisonAnalysisPackage extends React.Component {
 
     featureChange() {
         if (this.props.feature) {
-            if (this.props.feature.properties.userDefined) {
+            if(this.props.feature.properties.feature_id.includes('OBIS_Areas')){
+                this.props.isEnabled(false)
+                this.props.canOpen(false)
+            }
+            else if (this.props.feature.properties.userDefined) {
                 this.props.isEnabled(true)
                 this.props.canOpen(true)
                 this.setState({
