4d3d00895272cfe10a346b34ed34eb87da0e2cae
Josh Beard <jbeard@contractor.usgs.gov>
2019-05-10 11:38:23 -0600

    Update readme and run.sh for Docker
    * Update README - add steps for running in Docker container using the
    existing `run.sh` script.
    * Remove extraneous trailing whitespace from README
    * Modify the `run.sh` script to take a single argument - `static` - to
    run the static site similar to how the existing `run.sh` operated.
    Default to the non-static version.

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-docker-compose up --build
+if [ "$1" == "static" ]; then
+    set -x
+    docker-compose -f docker-compose.static.yml up --build
+else
+    set -x
+    docker-compose up --build
+fi
