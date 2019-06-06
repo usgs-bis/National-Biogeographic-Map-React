0f3d591a1da240cd693c3b7dacaf4779dc296c8b
Ben Gotthold <bgotthold@usgs.gov>
2019-06-06 13:03:16 -0600

    dont show search results when moving the time slider by comparing the point clicked to previos point. Also update search hnadeler to use feature id instead of id to be more consistant with how we handel features

--- src/AnalysisPackages/AnalysisPackage.js
+++ src/AnalysisPackages/AnalysisPackage.js
@@ -484,6 +484,7 @@ const withSharedAnalysisCharacteristics = (AnalysisPackage,
                                         <label htmlFor="prettyCheckbox">{"Pretty JSON "}  </label>
                                         <input checked={this.state.prettyJson}
                                             onChange={() => this.setState({ prettyJson: !this.state.prettyJson })}
+                                            onClick={() => this.setState({ prettyJson: !this.state.prettyJson })}
                                             type="checkbox" id="prettyCheckbox" name="prettyCheckbox" />
                                     </div>
                                     <div className="JSON-text-container">
