6418d941d25cdb95a580a7a776fdc377f67fde78
blohre <blohre@contractor.usgs.gov>
2019-12-31 12:59:00 -0700

    Update responsiveness
    Issue: BCB-1120

--- src/LeftPanel/LeftPanel.js
+++ src/LeftPanel/LeftPanel.js
@@ -173,62 +173,63 @@ class LeftPanel extends React.Component {
         return (
             <div className="left-panel">
                 <div id='left-panel-header' className="left-panel-header">
-                <div className="nbm-flex-row">
-                    <div className="mx-auto text-center">
-                        <span className="bioscape-title-text">{this.state.bioscape.title}</span>
-                        <span className="align-self-center">
-                            <InfoSign onClick={() => this.setState({ showDescription: !this.state.showDescription })}> </InfoSign>
-                        </span>
-                        { this.state.showDescription &&
-                            <div className="sbinfo-title">
-                                <CustomDialog
-                                    className="sbinfo-popout-window"
-                                    isResizable={true}
-                                    isDraggable={true}
-                                    title={this.state.bioscape.title}
-                                    modal={false}
-                                    onClose={() => this.setState({ showDescription: !this.state.showDescription })}
-                                    body={
-                                        <div className="sbinfo-body sbinfo-popout-window">{this.state.bioscape.description}</div>
-                                    }
-                                />
-                            </div>
-                        }
+                    <div className="nbm-flex-row">
+                        <div className="mx-auto text-center">
+                            <span className="bioscape-title-text">{this.state.bioscape.title}</span>
+                            <span className="align-self-center">
+                                <InfoSign onClick={() => this.setState({ showDescription: !this.state.showDescription })}> </InfoSign>
+                            </span>
+                            { this.state.showDescription &&
+                                <div className="sbinfo-title">
+                                    <CustomDialog
+                                        className="sbinfo-popout-window"
+                                        isResizable={true}
+                                        isDraggable={true}
+                                        title={this.state.bioscape.title}
+                                        modal={false}
+                                        onClose={() => this.setState({ showDescription: !this.state.showDescription })}
+                                        body={
+                                            <div className="sbinfo-body sbinfo-popout-window">{this.state.bioscape.description}</div>
+                                        }
+                                    />
+                                </div>
+                            }
+                        </div>
                     </div>
-                </div>
                     <SearchBar
                         {...this.props}
                         enabledLayers={this.state.enabledLayers}>
                     </SearchBar>
-                    {featureText()}
                 </div>
                 {this.state.displayHelpPopup && <div className="bap-popup" id="baphHelpPopup">
                     <img src={speechBubble} alt="Speech Bubble"></img>
                     <div className="bap-popuptext" id="myPopup">Choose an Analysis</div>
                 </div>}
+                <div className="analysis-area">
+                    {featureText()}
+                    <div id='analysis-package-container' className="analysis-package-container">
 
-                <div id='analysis-package-container' className="analysis-package-container">
+                        {this.state.feature_name && <div className="analysis-available">Analyses available for {this.state.feature_name}</div>}
+                        {!this.state.feature_name && <div className="analysis-package-text">Analysis Packages {this.state.feature_name}</div>}
+                        {
+                            this.props.bioscapeName === "terrestrial-ecosystems-2011" ?
+                                <TerrestrialEcosystems2011
+                                    onRef={ref => (this.TerrestrialEcosystems2011 = ref)}
+                                    {...this.props}
+                                    {...this.state}
+                                    updateAnalysisLayers={this.updateAnalysisLayers}
+                                />
+                                :
+                                <Biogeography
+                                    onRef={ref => (this.Biogeography = ref)}
+                                    {...this.props}
+                                    {...this.state}
+                                    updateAnalysisLayers={this.updateAnalysisLayers}
+                                />
 
-                    {this.state.feature_name && <div className="analysis-available">Analyses available for {this.state.feature_name}</div>}
-                    {!this.state.feature_name && <div className="analysis-package-text">Analysis Packages {this.state.feature_name}</div>}
-                    {
-                        this.props.bioscapeName === "terrestrial-ecosystems-2011" ?
-                            <TerrestrialEcosystems2011
-                                onRef={ref => (this.TerrestrialEcosystems2011 = ref)}
-                                {...this.props}
-                                {...this.state}
-                                updateAnalysisLayers={this.updateAnalysisLayers}
-                            />
-                            :
-                            <Biogeography
-                                onRef={ref => (this.Biogeography = ref)}
-                                {...this.props}
-                                {...this.state}
-                                updateAnalysisLayers={this.updateAnalysisLayers}
-                            />
-
-                    }
-                    <div id="d3chartTooltip" className='chartTooltip'></div>
+                        }
+                        <div id="d3chartTooltip" className='chartTooltip'></div>
+                    </div>
                 </div>
 
             </div>
